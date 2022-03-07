2851950bee2b5e8b9294404d60ad90f921cfba22
Yeap Yong Tuck <max@Yeaps-MacBook-Air.local>
2022-03-07 19:20:23 +0800

    Insert boilerplate HTML code to index.html & Add 4 different basic math functions to script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const add = function (x, y) {
+  const sum = x + y;
+  return sum;
+};
+
+const subtract = function (x, y) {
+  const sum = x - y;
+  return sum;
+};
+
+const multiply = function (x, y) {
+  const sum = x * y;
+  return sum;
+};
+
+const divide = function (x, y) {
+  const sum = x / y;
+  return sum;
+};
