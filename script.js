86a3af4e7e17dd92c0545c52a6a950027431ef79
Yeap Yong Tuck <max@Yeaps-MacBook-Air.local>
2022-03-12 13:44:13 +0800

    Add functional single digit calculation

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let displayValue;
+let calObj = {
+  initialNum: 0,
+  firstNum: 0,
+  secondNum: 0,
+  operator: "+",
+};
+
 const add = function (x, y) {
   const sum = x + y;
   return sum;
@@ -20,26 +28,79 @@ const divide = function (x, y) {
 
 const operate = function (o, a, b) {
   if (o == "+") {
-    const result = add(a, b);
-    return result;
+    if (calObj.firstNum == 0) {
+      const result = add(a, b);
+      displayValue = result;
+      calObj.firstNum += result;
+    } else {
+      const result = add(a, b);
+      displayValue = result;
+      calObj.firstNum = result;
+    }
+    document.getElementById("display").innerHTML = displayValue;
   } else if (o == "-") {
-    const result = subtract(a, b);
-    return result;
-  } else if (o == "*") {
-    const result = multiply(a, b);
-    return result;
-  } else if (o == "/") {
-    const result = divide(a, b);
-    return result;
-  } else {
-    return "Please input an operator!";
+    if (calObj.firstNum == 0) {
+      const result = subtract(a, b);
+      displayValue = result;
+      calObj.firstNum += result;
+    } else {
+      const result = subtract(a, b);
+      displayValue = result;
+      calObj.firstNum = result;
+    }
+    document.getElementById("display").innerHTML = displayValue;
+  } else if (o == "x") {
+    if (calObj.firstNum == 0) {
+      const result = multiply(a, b);
+      displayValue = result;
+      calObj.firstNum += result;
+    } else {
+      const result = multiply(a, b);
+      displayValue = result;
+      calObj.firstNum = result;
+    }
+    document.getElementById("display").innerHTML = displayValue;
+  } else if (o == "÷") {
+    if (calObj.firstNum == 0) {
+      const result = divide(a, b);
+      displayValue = result;
+      calObj.firstNum += result;
+    } else {
+      const result = divide(a, b);
+      displayValue = result;
+      calObj.firstNum = result;
+    }
+    document.getElementById("display").innerHTML = displayValue;
   }
 };
 
-let displayValue = 0;
-
-document.getElementById("display").innerHTML = displayValue;
-
 const displayNumber = function (num) {
+  if (calObj.initialNum == 0) {
+    calObj.initialNum = num;
+  } else if (calObj.initialNum > 0) {
+    calObj.secondNum = num;
+  }
   displayValue = num;
+  document.getElementById("display").innerHTML = displayValue;
 };
+
+const equalFunc = function (input) {
+  if (input == "=" && calObj.firstNum > 0 && calObj.initialNum > 0) {
+    operate(calObj.operator, calObj.firstNum, calObj.secondNum);
+  } else if (input == "=" && calObj.initialNum > 0 && calObj.firstNum == 0) {
+    operate(calObj.operator, calObj.initialNum, calObj.secondNum);
+  }
+};
+
+const operatorFunc = function (input) {
+  calObj.operator = input;
+};
+
+// operator
+// First num, second num,
+
+// first num = 1
+// operator = "+"
+// second = 2
+// run function operate when click =
+// display value
